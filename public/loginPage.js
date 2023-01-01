f25890612bee9c174131834e079e128fcbd1035a
Ludmila <lvminzulina@mail.ru>
2023-01-01 16:23:07 +0300

    login

---
+++ public/loginPage.js
@@ -0,0 +1,29 @@
+'use strict'
+let userForm = new UserForm();
+userForm.loginFormCallback = function f(data) {
+    if (data !== null) {
+        ApiConnector.login({
+            login: data.login,
+            password: data.password
+        }, (response) => {
+            console.log(response);
+            response.success ? location.reload() : userForm.setLoginErrorMessage(response.error);
+        });
+    }
+};
+
+userForm.registerFormCallback = function f(data) {
+    if (data !== null) {
+        ApiConnector.register({
+                login: data.login,
+                password: data.password
+            },
+            (response) => {
+                console.log(response);
+                response.success == true ?
+                    location.reload() :
+                    userForm.setRegisterErrorMessage(response.error);
+            }
+        );
+    }
+};
